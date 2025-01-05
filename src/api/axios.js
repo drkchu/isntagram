cc65940f18429f1a33fee26511a69edd523a456b
drkchu <derekdbchu@gmail.com>
2025-01-04 20:52:35 -0800

    feat: add api environment variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Backend url
+  baseURL: process.env.API_URL || "http://localhost:3000", // Backend url
 });
 
 api.interceptors.request.use((config) => {
