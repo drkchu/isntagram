904f5610ae32df46f6db9d2eae051e77f67d3905
drkchu <derekdbchu@gmail.com>
2025-01-04 21:16:26 -0800

    fix: rename environment variable cuz react won't expose otherwise

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:3000", // Backend url
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000", // Backend url
 });
 
 api.interceptors.request.use((config) => {
